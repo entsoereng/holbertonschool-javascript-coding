4b29809ede6cba31c88acf67adb8c75cf09d2e59
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-05-15 10:45:52 +0200

    Update 1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-const fs = require('fs');
-const filePath = process.argv[2];
-const stringToWrite = process.argv[3];
 
-fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('The file has been saved!');
-    }
+const fs = require('fs');
+const fileName = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(fileName, data, 'utf-8', (error) => {
+  if (error) {
+    console.error(error);
+  }
 });
