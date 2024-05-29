6016069db72e664998dfa56c2dc01693d5dced29
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-05-29 13:06:41 +0200

    Update 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name.toString()}`);
+});
+process.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
-  process.exit();
 });
