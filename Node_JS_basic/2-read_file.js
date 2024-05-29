6c92f71f116969e159a1d0add97964a1ce20c520
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-05-29 13:08:02 +0200

    Update 2-read_file.js

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,44 +1,39 @@
+
 const fs = require('fs');
 
 function countStudents(path) {
-  try {
-    // Read the file content synchronously
-    const data = fs.readFileSync(path, 'utf8');
-    
-    // Split the file content by new lines to get each row
-    const lines = data.split('\n').filter(line => line.trim() !== '');
-
-    if (lines.length === 0) {
-      console.log('Number of students: 0');
-      return;
-    }
-
-    // Remove the header row
-    const header = lines.shift();
-
-    const students = {};
-    let totalStudents = 0;
-
-    lines.forEach(line => {
-      const [firstName, , , field] = line.split(',');
-
-      if (firstName && field) {
-        if (!students[field]) {
-          students[field] = [];
-        }
-        students[field].push(firstName);
-        totalStudents++;
-      }
-    });
-
-    console.log(`Number of students: ${totalStudents}`);
-
-    for (const [field, names] of Object.entries(students)) {
-      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
-    }
-
-  } catch (error) {
+  if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
+  }
+  const data = fs.readFileSync(path, 'utf8').split('\n');
+  if (data.length === 0) {
+    throw new Error('Cannot load the database');
+  }
+
+  const students = data.map((line) => line.split(','));
+  console.log(`Number of students: ${students.slice(1).length - 1}`);
+
+  const fields = students[0];
+  const fieldIndex = fields.indexOf('field');
+
+  if (fieldIndex === -1) {
+    throw new Error('Field column not found');
+  }
+  const validStudents = students
+    .slice(1)
+    .filter((student) => student[fieldIndex] !== undefined && student[fieldIndex] !== '');
+
+  const classes = validStudents.reduce((acc, student) => {
+    const className = student[fieldIndex];
+    if (!acc[className]) {
+      acc[className] = [];
+    }
+    acc[className].push(student[fields.indexOf('firstname')]);
+    return acc;
+  }, {});
+
+  for (const [className, studentList] of Object.entries(classes)) {
+    console.log(`Number of students in ${className}: ${studentList.length}. List: ${studentList.join(', ')}`);
   }
 }
 
